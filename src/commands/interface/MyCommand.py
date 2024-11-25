5404637df5b23e0d4bf6e03dc720d8d0b3d65921
Bradley Mize <bradley.mize@gmail.com>
2024-11-24 22:24:47 -0800

    - Updated MyCommand to add some instance properties that need to be populated - Updated MyCommand to add a function to register the command given instance properties - Updated the test2 command to use the newly added members of the abstract class to make registering   a new command more uniform / easier to test

--- src/commands/interface/MyCommand.py
+++ src/commands/interface/MyCommand.py
@@ -1,7 +1,25 @@
 from abc import ABC, abstractmethod
 import discord
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class MyCommand(ABC):
+    bot = None
+    cmd_meta = {}
+    options = []
+    fn = None
+
     @abstractmethod
     def init(self):
         pass
+
+    def register_command(self):
+        log.info(f"Creating command: {self.cmd_meta['name']}")
+        for option in list(reversed(self.options)):
+            log.info(f"    Registering option: {option['name']}")
+            self.fn = discord.option(**option)(self.fn)
+
+        self.bot.command(**self.cmd_meta)(self.fn)
+        log.info(f"  Done creating command: {self.cmd_meta['name']}")
--- src/commands/test2.py
+++ src/commands/test2.py
@@ -7,10 +7,75 @@ class TestTwo(MyCommand):
 
 
     def init(self):
-        print("Initializing Test 2 - test2 command")
-        self.bot.command(name="test2", description="Simple command")(self.command)
+        self.cmd_meta = {
+            'name': 'test2',
+            'description': 'A complex but basic command'
+        }
+        self.options = [
+            {
+                'name': 'prompt',
+                'type': discord.SlashCommandOptionType.string,
+                'required': True,
+                'description': "Describe the image you wish to create"
+            },
+            {
+                'name': 'workflow',
+                'type': discord.SlashCommandOptionType.string,
+                'required': True,
+                'description': "The workflow to use for generating the image",
+                'default': "default.json.template",
+                'autocomplete': discord.utils.basic_autocomplete(self.get_workflows)
+            },
+            {
+                'name': 'seed',
+                'type': discord.SlashCommandOptionType.string,
+                'required': False,
+                'description': "Number used to help re-create images. Default: (random)"
+            },
+            {
+                'name': 'width',
+                'type': discord.SlashCommandOptionType.integer,
+                'required': False,
+                'description': "The desired width of the generated image. Default: 1024"
+            },
+            {
+                'name': 'height',
+                'type': discord.SlashCommandOptionType.integer,
+                'required': False,
+                'description': "The desired height of the generated image. Default: 1024"
+            },
+            {
+                'name': 'steps',
+                'type': discord.SlashCommandOptionType.integer,
+                'required': False,
+                'description': "The number of iterations to perform when generating the image. Default: 4"
+            }
+        ]
+        self.fn = self.command
+        super().register_command()
 
+    def get_workflows(
+            self,
+            ctx: discord.commands.context.AutocompleteContext
+    ):
+        return ["foo", "bar"]
 
-    async def command(self, ctx: discord.ApplicationContext):
-        print("Inside Test 2 - command")
-        await ctx.respond("NOT Test 1, but Test 2")
+    async def command(
+            self,
+            ctx: discord.ApplicationContext,
+            prompt: str,
+            workflow: str = "default.json.template",
+            seed: str = None,
+            width: int = 1024,
+            height: int = 1024,
+            steps: int = 4
+    ):
+        msg = f"""Received arguments:
+workflow: {workflow}
+seed: {seed}
+width: {width}
+height: {height}
+steps: {steps}
+prompt: {prompt}
+"""
+        await ctx.respond(msg)
